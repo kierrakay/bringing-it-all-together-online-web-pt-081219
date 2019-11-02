1dbdfd0f1f2ea51999dd1f7b92178e8e9fde24e5
Kierra M <k.merriman32@hotmail.com>
2019-11-02 14:38:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,13 +27,7 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-  def save 
-      sql = <<-SQL 
-    INSERT INTO dogs (name, breed)
-    VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql,self.name, self.breed) #this creates the entry
-   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs").first.first
+   def self.save (name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type) 
   end
-  
 end
